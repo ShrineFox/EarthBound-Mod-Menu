0472b306ff0607582692c0efe56312caa42bad72
ShrineFox <ShrineFox@users.noreply.github.com>
2024-08-03 19:33:46 -0400

    Add icon to Chaos Mode editor form

--- ChaosModeEditor/ChaosModeEditorForm.Designer.cs
+++ ChaosModeEditor/ChaosModeEditorForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChaosModeEditor));
             this.tlp_Main = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox_RandomBBG = new System.Windows.Forms.GroupBox();
             this.num_RandomBBG = new System.Windows.Forms.NumericUpDown();
@@ -54,6 +55,7 @@
             this.btn_Update = new System.Windows.Forms.Button();
             this.groupBox_RandomPalette = new System.Windows.Forms.GroupBox();
             this.num_RandomPalette = new System.Windows.Forms.NumericUpDown();
+            this.lbl_Help = new System.Windows.Forms.Label();
             this.tlp_Main.SuspendLayout();
             this.groupBox_RandomBBG.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.num_RandomBBG)).BeginInit();
@@ -99,6 +101,7 @@
             this.tlp_Main.Controls.Add(this.groupBox_RandomBattle, 0, 0);
             this.tlp_Main.Controls.Add(this.groupBox_CorruptMap, 1, 0);
             this.tlp_Main.Controls.Add(this.btn_Update, 1, 6);
+            this.tlp_Main.Controls.Add(this.lbl_Help, 0, 6);
             this.tlp_Main.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlp_Main.Location = new System.Drawing.Point(0, 0);
             this.tlp_Main.Name = "tlp_Main";
@@ -110,7 +113,7 @@
             this.tlp_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tlp_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tlp_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tlp_Main.Size = new System.Drawing.Size(604, 405);
+            this.tlp_Main.Size = new System.Drawing.Size(607, 403);
             this.tlp_Main.TabIndex = 0;
             // 
             // groupBox_RandomBBG
@@ -120,7 +123,7 @@
             this.groupBox_RandomBBG.ForeColor = System.Drawing.SystemColors.ControlText;
             this.groupBox_RandomBBG.Location = new System.Drawing.Point(3, 303);
             this.groupBox_RandomBBG.Name = "groupBox_RandomBBG";
-            this.groupBox_RandomBBG.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomBBG.Size = new System.Drawing.Size(297, 54);
             this.groupBox_RandomBBG.TabIndex = 11;
             this.groupBox_RandomBBG.TabStop = false;
             this.groupBox_RandomBBG.Text = "Random Battle BG";
@@ -128,7 +131,7 @@
             // num_RandomBBG
             // 
             this.num_RandomBBG.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomBBG.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomBBG.Location = new System.Drawing.Point(174, 18);
             this.num_RandomBBG.Maximum = new decimal(new int[] {
             255,
             0,
@@ -143,9 +146,9 @@
             this.groupBox_RandomDistortion.Controls.Add(this.num_RandomDistortion);
             this.groupBox_RandomDistortion.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox_RandomDistortion.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.groupBox_RandomDistortion.Location = new System.Drawing.Point(305, 243);
+            this.groupBox_RandomDistortion.Location = new System.Drawing.Point(306, 243);
             this.groupBox_RandomDistortion.Name = "groupBox_RandomDistortion";
-            this.groupBox_RandomDistortion.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomDistortion.Size = new System.Drawing.Size(298, 54);
             this.groupBox_RandomDistortion.TabIndex = 10;
             this.groupBox_RandomDistortion.TabStop = false;
             this.groupBox_RandomDistortion.Text = "Random Distortion";
@@ -153,7 +156,7 @@
             // num_RandomDistortion
             // 
             this.num_RandomDistortion.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomDistortion.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomDistortion.Location = new System.Drawing.Point(175, 18);
             this.num_RandomDistortion.Maximum = new decimal(new int[] {
             255,
             0,
@@ -170,7 +173,7 @@
             this.groupBox_RandomSound.ForeColor = System.Drawing.SystemColors.ControlText;
             this.groupBox_RandomSound.Location = new System.Drawing.Point(3, 243);
             this.groupBox_RandomSound.Name = "groupBox_RandomSound";
-            this.groupBox_RandomSound.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomSound.Size = new System.Drawing.Size(297, 54);
             this.groupBox_RandomSound.TabIndex = 9;
             this.groupBox_RandomSound.TabStop = false;
             this.groupBox_RandomSound.Text = "Random Sound";
@@ -178,7 +181,7 @@
             // num_RandomSound
             // 
             this.num_RandomSound.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomSound.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomSound.Location = new System.Drawing.Point(174, 18);
             this.num_RandomSound.Maximum = new decimal(new int[] {
             255,
             0,
@@ -193,9 +196,9 @@
             this.groupBox_RandomizeNpcParty.Controls.Add(this.num_RandomizeNpcParty);
             this.groupBox_RandomizeNpcParty.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox_RandomizeNpcParty.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.groupBox_RandomizeNpcParty.Location = new System.Drawing.Point(305, 183);
+            this.groupBox_RandomizeNpcParty.Location = new System.Drawing.Point(306, 183);
             this.groupBox_RandomizeNpcParty.Name = "groupBox_RandomizeNpcParty";
-            this.groupBox_RandomizeNpcParty.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomizeNpcParty.Size = new System.Drawing.Size(298, 54);
             this.groupBox_RandomizeNpcParty.TabIndex = 8;
             this.groupBox_RandomizeNpcParty.TabStop = false;
             this.groupBox_RandomizeNpcParty.Text = "Randomize NPC Party";
@@ -203,7 +206,7 @@
             // num_RandomizeNpcParty
             // 
             this.num_RandomizeNpcParty.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomizeNpcParty.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomizeNpcParty.Location = new System.Drawing.Point(175, 18);
             this.num_RandomizeNpcParty.Maximum = new decimal(new int[] {
             255,
             0,
@@ -220,7 +223,7 @@
             this.groupBox_RandomizeMainParty.ForeColor = System.Drawing.SystemColors.ControlText;
             this.groupBox_RandomizeMainParty.Location = new System.Drawing.Point(3, 183);
             this.groupBox_RandomizeMainParty.Name = "groupBox_RandomizeMainParty";
-            this.groupBox_RandomizeMainParty.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomizeMainParty.Size = new System.Drawing.Size(297, 54);
             this.groupBox_RandomizeMainParty.TabIndex = 7;
             this.groupBox_RandomizeMainParty.TabStop = false;
             this.groupBox_RandomizeMainParty.Text = "Randomize Main Party";
@@ -228,7 +231,7 @@
             // num_RandomizeMainParty
             // 
             this.num_RandomizeMainParty.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomizeMainParty.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomizeMainParty.Location = new System.Drawing.Point(174, 18);
             this.num_RandomizeMainParty.Maximum = new decimal(new int[] {
             255,
             0,
@@ -242,9 +245,9 @@
             // 
             this.groupBox_BacktrackWarp.Controls.Add(this.num_BacktrackWarp);
             this.groupBox_BacktrackWarp.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox_BacktrackWarp.Location = new System.Drawing.Point(305, 123);
+            this.groupBox_BacktrackWarp.Location = new System.Drawing.Point(306, 123);
             this.groupBox_BacktrackWarp.Name = "groupBox_BacktrackWarp";
-            this.groupBox_BacktrackWarp.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_BacktrackWarp.Size = new System.Drawing.Size(298, 54);
             this.groupBox_BacktrackWarp.TabIndex = 6;
             this.groupBox_BacktrackWarp.TabStop = false;
             this.groupBox_BacktrackWarp.Text = "Backtrack Warp";
@@ -252,7 +255,7 @@
             // num_BacktrackWarp
             // 
             this.num_BacktrackWarp.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_BacktrackWarp.Location = new System.Drawing.Point(173, 18);
+            this.num_BacktrackWarp.Location = new System.Drawing.Point(175, 18);
             this.num_BacktrackWarp.Maximum = new decimal(new int[] {
             255,
             0,
@@ -268,7 +271,7 @@
             this.groupBox_RandomFloatSprite.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox_RandomFloatSprite.Location = new System.Drawing.Point(3, 123);
             this.groupBox_RandomFloatSprite.Name = "groupBox_RandomFloatSprite";
-            this.groupBox_RandomFloatSprite.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomFloatSprite.Size = new System.Drawing.Size(297, 54);
             this.groupBox_RandomFloatSprite.TabIndex = 5;
             this.groupBox_RandomFloatSprite.TabStop = false;
             this.groupBox_RandomFloatSprite.Text = "Random Float Sprite";
@@ -276,7 +279,7 @@
             // num_RandomFloatSprite
             // 
             this.num_RandomFloatSprite.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomFloatSprite.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomFloatSprite.Location = new System.Drawing.Point(174, 18);
             this.num_RandomFloatSprite.Maximum = new decimal(new int[] {
             255,
             0,
@@ -290,9 +293,9 @@
             // 
             this.groupBox_RandomBGM.Controls.Add(this.num_RandomBGM);
             this.groupBox_RandomBGM.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox_RandomBGM.Location = new System.Drawing.Point(305, 63);
+            this.groupBox_RandomBGM.Location = new System.Drawing.Point(306, 63);
             this.groupBox_RandomBGM.Name = "groupBox_RandomBGM";
-            this.groupBox_RandomBGM.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomBGM.Size = new System.Drawing.Size(298, 54);
             this.groupBox_RandomBGM.TabIndex = 4;
             this.groupBox_RandomBGM.TabStop = false;
             this.groupBox_RandomBGM.Text = "Random BGM";
@@ -300,7 +303,7 @@
             // num_RandomBGM
             // 
             this.num_RandomBGM.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomBGM.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomBGM.Location = new System.Drawing.Point(175, 18);
             this.num_RandomBGM.Maximum = new decimal(new int[] {
             255,
             0,
@@ -316,7 +319,7 @@
             this.groupBox_RandomWarp.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox_RandomWarp.Location = new System.Drawing.Point(3, 63);
             this.groupBox_RandomWarp.Name = "groupBox_RandomWarp";
-            this.groupBox_RandomWarp.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomWarp.Size = new System.Drawing.Size(297, 54);
             this.groupBox_RandomWarp.TabIndex = 3;
             this.groupBox_RandomWarp.TabStop = false;
             this.groupBox_RandomWarp.Text = "Random Warp";
@@ -324,7 +327,7 @@
             // num_RandomWarp
             // 
             this.num_RandomWarp.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomWarp.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomWarp.Location = new System.Drawing.Point(174, 18);
             this.num_RandomWarp.Maximum = new decimal(new int[] {
             255,
             0,
@@ -340,7 +343,7 @@
             this.groupBox_RandomBattle.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox_RandomBattle.Location = new System.Drawing.Point(3, 3);
             this.groupBox_RandomBattle.Name = "groupBox_RandomBattle";
-            this.groupBox_RandomBattle.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomBattle.Size = new System.Drawing.Size(297, 54);
             this.groupBox_RandomBattle.TabIndex = 1;
             this.groupBox_RandomBattle.TabStop = false;
             this.groupBox_RandomBattle.Text = "Random Battle";
@@ -348,7 +351,7 @@
             // num_RandomBattle
             // 
             this.num_RandomBattle.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomBattle.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomBattle.Location = new System.Drawing.Point(174, 18);
             this.num_RandomBattle.Maximum = new decimal(new int[] {
             255,
             0,
@@ -362,9 +365,9 @@
             // 
             this.groupBox_CorruptMap.Controls.Add(this.num_CorruptMap);
             this.groupBox_CorruptMap.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox_CorruptMap.Location = new System.Drawing.Point(305, 3);
+            this.groupBox_CorruptMap.Location = new System.Drawing.Point(306, 3);
             this.groupBox_CorruptMap.Name = "groupBox_CorruptMap";
-            this.groupBox_CorruptMap.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_CorruptMap.Size = new System.Drawing.Size(298, 54);
             this.groupBox_CorruptMap.TabIndex = 2;
             this.groupBox_CorruptMap.TabStop = false;
             this.groupBox_CorruptMap.Text = "Corrupt Map";
@@ -372,7 +375,7 @@
             // num_CorruptMap
             // 
             this.num_CorruptMap.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_CorruptMap.Location = new System.Drawing.Point(173, 18);
+            this.num_CorruptMap.Location = new System.Drawing.Point(175, 18);
             this.num_CorruptMap.Maximum = new decimal(new int[] {
             255,
             0,
@@ -386,9 +389,9 @@
             // 
             this.btn_Update.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btn_Update.Location = new System.Drawing.Point(305, 363);
+            this.btn_Update.Location = new System.Drawing.Point(306, 363);
             this.btn_Update.Name = "btn_Update";
-            this.btn_Update.Size = new System.Drawing.Size(296, 39);
+            this.btn_Update.Size = new System.Drawing.Size(298, 37);
             this.btn_Update.TabIndex = 12;
             this.btn_Update.Text = "Update";
             this.btn_Update.UseVisualStyleBackColor = true;
@@ -399,9 +402,9 @@
             this.groupBox_RandomPalette.Controls.Add(this.num_RandomPalette);
             this.groupBox_RandomPalette.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox_RandomPalette.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.groupBox_RandomPalette.Location = new System.Drawing.Point(305, 303);
+            this.groupBox_RandomPalette.Location = new System.Drawing.Point(306, 303);
             this.groupBox_RandomPalette.Name = "groupBox_RandomPalette";
-            this.groupBox_RandomPalette.Size = new System.Drawing.Size(296, 54);
+            this.groupBox_RandomPalette.Size = new System.Drawing.Size(298, 54);
             this.groupBox_RandomPalette.TabIndex = 13;
             this.groupBox_RandomPalette.TabStop = false;
             this.groupBox_RandomPalette.Text = "Random Palette";
@@ -409,7 +412,7 @@
             // num_RandomPalette
             // 
             this.num_RandomPalette.Dock = System.Windows.Forms.DockStyle.Right;
-            this.num_RandomPalette.Location = new System.Drawing.Point(173, 18);
+            this.num_RandomPalette.Location = new System.Drawing.Point(175, 18);
             this.num_RandomPalette.Maximum = new decimal(new int[] {
             255,
             0,
@@ -419,15 +422,29 @@
             this.num_RandomPalette.Size = new System.Drawing.Size(120, 22);
             this.num_RandomPalette.TabIndex = 0;
             // 
+            // lbl_Help
+            // 
+            this.lbl_Help.AutoSize = true;
+            this.lbl_Help.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_Help.Location = new System.Drawing.Point(3, 360);
+            this.lbl_Help.Name = "lbl_Help";
+            this.lbl_Help.Size = new System.Drawing.Size(297, 43);
+            this.lbl_Help.TabIndex = 14;
+            this.lbl_Help.Text = "Larger difference between numbers = higher probability. Build with CoilSnake afte" +
+    "r editing!";
+            // 
             // ChaosModeEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 405);
+            this.ClientSize = new System.Drawing.Size(607, 403);
             this.Controls.Add(this.tlp_Main);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(625, 450);
             this.Name = "ChaosModeEditor";
             this.Text = "EBModMenu Chaos Mode Editor";
             this.tlp_Main.ResumeLayout(false);
+            this.tlp_Main.PerformLayout();
             this.groupBox_RandomBBG.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.num_RandomBBG)).EndInit();
             this.groupBox_RandomDistortion.ResumeLayout(false);
@@ -484,6 +501,7 @@
         private System.Windows.Forms.Button btn_Update;
         private System.Windows.Forms.GroupBox groupBox_RandomPalette;
         private System.Windows.Forms.NumericUpDown num_RandomPalette;
+        private System.Windows.Forms.Label lbl_Help;
     }
 }
 
